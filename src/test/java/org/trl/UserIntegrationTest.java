746d34d1a86f9669512d777adc095c82a343efa4
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2021-01-29 11:51:57 +0100

    refactor() adding littles changes (master)

--- src/test/java/org/trl/UserIntegrationTest.java
+++ src/test/java/org/trl/UserIntegrationTest.java
@@ -1,30 +1,16 @@
 package org.trl;
 
-import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.mockito.InjectMock;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.trl.controller.dto.ApiErrorDto;
-import org.trl.exception.UserNotFoundException;
-import org.trl.service.DateTimeService;
-import org.trl.service.UserService;
-
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-import javax.ws.rs.core.MediaType;
-import java.net.HttpURLConnection;
-import java.time.LocalDateTime;
+import org.trl.model.dto.ApiErrorDto;
 
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.when;
 
-@QuarkusTest
+//@QuarkusTest
 public class UserIntegrationTest {
 
-    @InjectMock
-    DateTimeService dateTimeServiceMock;
+//    @InjectMock
+//    DateTimeService dateTimeServiceMock;
 
 //    @Test
 //    void create() {
@@ -62,149 +48,105 @@ public class UserIntegrationTest {
 //                .body(is(bodyJSONResponse));
 //    }
 
-    @Test
-    void get() {
-        long userId = 1L;
-        final String bodyJSONResponse = "{\"birthday\":\"1988-06-26\",\"email\":\"tsyupryk.roman@gmail.com\",\"firstName\":\"Roman\",\"id\":1,\"lastName\":\"Tsyupryk\",\"password\":\"strong_password\",\"username\":\"TRL\"}";
-        given()
-                .pathParam("id", userId)
-                .when().get("/users/{id}")
-                .then()
-                .statusCode(HttpURLConnection.HTTP_OK)
-                .body(is(bodyJSONResponse));
-    }
+//    @Test
+//    void update() {
+//        final String bodyJSONRequest = "{\"birthday\":\"2000-01-18\",\"email\":\"8l@mail.com\",\"firstName\":\"8Carlos\",\"lastName\":\"8Rodriguez\",\"password\":\"8strong_password\",\"username\":\"8cr\"}";
+//        long userId = 2L;
+//        final String bodyJSONResponse = "{\"birthday\":\"2000-01-18\",\"email\":\"8l@mail.com\",\"firstName\":\"8Carlos\",\"id\":2,\"lastName\":\"8Rodriguez\",\"password\":\"8strong_password\",\"username\":\"8cr\"}";
+//        given()
+//                .pathParam("id", userId)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(bodyJSONRequest)
+//                .when().patch("/users/{id}")
+//                .then()
+//                .statusCode(HttpURLConnection.HTTP_OK)
+//                .body(is(bodyJSONResponse));
+//    }
 
-    @Test
-    void get_Illegal_UserId() {
-        long userId = 0L;
-        final String bodyJSONResponse = "{\"exceptionType\":\"java.lang.IllegalArgumentException\",\"statusCode\":422,\"exceptionMessage\":\"One of parameters is illegal. Parameters must be not equals to null, and parameters must be greater that zero. Check the parameter that are passed to the method.\"}";
-        given()
-                .pathParam("id", userId)
-                .when().get("/users/{id}")
-                .then()
-                .statusCode(422)
-                .body(is(bodyJSONResponse));
-    }
+//    @Test
+//    void update_Illegal_UserId() {
+//        final String bodyJSONRequest = "{\"birthday\":\"2000-01-18\",\"email\":\"8l@mail.com\",\"firstName\":\"8Carlos\",\"lastName\":\"8Rodriguez\",\"password\":\"8strong_password\",\"username\":\"8cr\"}";
+//        long userId = 0L;
+//        final String bodyJSONResponse = "{\"exceptionType\":\"java.lang.IllegalArgumentException\",\"statusCode\":422,\"exceptionMessage\":\"One of parameters is illegal. Parameters must be not equals to null, and parameters must be greater that zero. Check the parameter that are passed to the method.\"}";
+//        given()
+//                .pathParam("id", userId)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(bodyJSONRequest)
+//                .when().patch("/users/{id}")
+//                .then()
+//                .statusCode(422)
+//                .body(is(bodyJSONResponse));
+//    }
 
-    @Test
-    void whenTheUserIsNotFound_shouldThrowUserNotFoundException() {
-        final long userId = 100L;
-        LocalDateTime dateTime = LocalDateTime.of(2020, 6, 12, 10, 35, 53);
-        UserNotFoundException exception = new UserNotFoundException(format("User with this userId = %s not exist.", userId));
-        ApiErrorDto errorDto = new ApiErrorDto.Builder()
-                .withTimestamp(dateTime)
-                .withErrorMessage(exception.getMessage())
-                .withPath("")
-                .build();
+//    @Test
+//    void update_UserById_NotFound() {
+//        final String bodyJSONRequest = "{\"birthday\":\"2000-01-18\",\"email\":\"8l@mail.com\",\"firstName\":\"8Carlos\",\"lastName\":\"8Rodriguez\",\"password\":\"8strong_password\",\"username\":\"8cr\"}";
+//        final long userId = 100L;
+//        LocalDateTime dateTime = LocalDateTime.of(2020, 6, 12, 10, 35, 53);
+//        UserNotFoundException exception = new UserNotFoundException(format("User with this userId = %s not exist.", userId));
+//        ApiErrorDto errorDto = new ApiErrorDto.Builder()
+//                .withTimestamp(dateTime)
+//                .withErrorMessage(exception.getMessage())
+//                .withPath("")
+//                .build();
+//
+//        final String expectedJsonResponse = JsonbBuilder.create().toJson(errorDto);
+//
+//        when(dateTimeServiceMock.now()).thenReturn(dateTime);
+//
+//        given()
+//                .pathParam("id", userId)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(bodyJSONRequest)
+//                .when().patch("/users/{id}")
+//                .then()
+//                .statusCode(HttpURLConnection.HTTP_NOT_FOUND)
+//                .body(is(expectedJsonResponse));
+//    }
 
-        final String expectedJsonResponse = JsonbBuilder.create().toJson(errorDto);
-
-        when(dateTimeServiceMock.now()).thenReturn(dateTime);
-
-        given().pathParam("id", userId)
-                .when().get("/users/{id}")
-                .then().statusCode(HttpURLConnection.HTTP_NOT_FOUND).body(is(expectedJsonResponse));
-    }
-
-    @Test
-    void update() {
-        final String bodyJSONRequest = "{\"birthday\":\"2000-01-18\",\"email\":\"8l@mail.com\",\"firstName\":\"8Carlos\",\"lastName\":\"8Rodriguez\",\"password\":\"8strong_password\",\"username\":\"8cr\"}";
-        long userId = 2L;
-        final String bodyJSONResponse = "{\"birthday\":\"2000-01-18\",\"email\":\"8l@mail.com\",\"firstName\":\"8Carlos\",\"id\":2,\"lastName\":\"8Rodriguez\",\"password\":\"8strong_password\",\"username\":\"8cr\"}";
-        given()
-                .pathParam("id", userId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(bodyJSONRequest)
-                .when().patch("/users/{id}")
-                .then()
-                .statusCode(HttpURLConnection.HTTP_OK)
-                .body(is(bodyJSONResponse));
-    }
-
-    @Test
-    void update_Illegal_UserId() {
-        final String bodyJSONRequest = "{\"birthday\":\"2000-01-18\",\"email\":\"8l@mail.com\",\"firstName\":\"8Carlos\",\"lastName\":\"8Rodriguez\",\"password\":\"8strong_password\",\"username\":\"8cr\"}";
-        long userId = 0L;
-        final String bodyJSONResponse = "{\"exceptionType\":\"java.lang.IllegalArgumentException\",\"statusCode\":422,\"exceptionMessage\":\"One of parameters is illegal. Parameters must be not equals to null, and parameters must be greater that zero. Check the parameter that are passed to the method.\"}";
-        given()
-                .pathParam("id", userId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(bodyJSONRequest)
-                .when().patch("/users/{id}")
-                .then()
-                .statusCode(422)
-                .body(is(bodyJSONResponse));
-    }
-
-    @Test
-    void update_UserById_NotFound() {
-        final String bodyJSONRequest = "{\"birthday\":\"2000-01-18\",\"email\":\"8l@mail.com\",\"firstName\":\"8Carlos\",\"lastName\":\"8Rodriguez\",\"password\":\"8strong_password\",\"username\":\"8cr\"}";
-        final long userId = 100L;
-        LocalDateTime dateTime = LocalDateTime.of(2020, 6, 12, 10, 35, 53);
-        UserNotFoundException exception = new UserNotFoundException(format("User with this userId = %s not exist.", userId));
-        ApiErrorDto errorDto = new ApiErrorDto.Builder()
-                .withTimestamp(dateTime)
-                .withErrorMessage(exception.getMessage())
-                .withPath("")
-                .build();
-
-        final String expectedJsonResponse = JsonbBuilder.create().toJson(errorDto);
-
-        when(dateTimeServiceMock.now()).thenReturn(dateTime);
-
-        given()
-                .pathParam("id", userId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(bodyJSONRequest)
-                .when().patch("/users/{id}")
-                .then()
-                .statusCode(HttpURLConnection.HTTP_NOT_FOUND)
-                .body(is(expectedJsonResponse));
-    }
-
-    @Test
-    void delete() {
-        long userId = 5L;
-        given()
-                .pathParam("id", userId)
-                .when().delete("/users/{id}")
-                .then()
-                .statusCode(204);
-    }
-
-    @Test
-    void delete_Illegal_UserId() {
-        long userId = 0L;
-        final String bodyJSONResponse = "{\"exceptionType\":\"java.lang.IllegalArgumentException\",\"statusCode\":422,\"exceptionMessage\":\"One of parameters is illegal. Parameters must be not equals to null, and parameters must be greater that zero. Check the parameter that are passed to the method.\"}";
-        given()
-                .pathParam("id", userId)
-                .when().delete("/users/{id}")
-                .then()
-                .statusCode(422)
-                .body(is(bodyJSONResponse));
-    }
-
-    @Test
-    void delete_UserById_NotFound() {
-        final long userId = 100L;
-        LocalDateTime dateTime = LocalDateTime.of(2020, 6, 12, 10, 35, 53);
-        UserNotFoundException exception = new UserNotFoundException(format("User with this userId = %s not exist.", userId));
-        ApiErrorDto errorDto = new ApiErrorDto.Builder()
-                .withTimestamp(dateTime)
-                .withErrorMessage(exception.getMessage())
-                .withPath("")
-                .build();
-
-        final String expectedJsonResponse = JsonbBuilder.create().toJson(errorDto);
-
-        when(dateTimeServiceMock.now()).thenReturn(dateTime);
-
-        given()
-                .pathParam("id", userId)
-                .when().delete("/users/{id}")
-                .then()
-                .statusCode(HttpURLConnection.HTTP_NOT_FOUND)
-                .body(is(expectedJsonResponse));
-    }
+//    @Test
+//    void delete() {
+//        long userId = 5L;
+//        given()
+//                .pathParam("id", userId)
+//                .when().delete("/users/{id}")
+//                .then()
+//                .statusCode(204);
+//    }
+//
+//    @Test
+//    void delete_Illegal_UserId() {
+//        long userId = 0L;
+//        final String bodyJSONResponse = "{\"exceptionType\":\"java.lang.IllegalArgumentException\",\"statusCode\":422,\"exceptionMessage\":\"One of parameters is illegal. Parameters must be not equals to null, and parameters must be greater that zero. Check the parameter that are passed to the method.\"}";
+//        given()
+//                .pathParam("id", userId)
+//                .when().delete("/users/{id}")
+//                .then()
+//                .statusCode(422)
+//                .body(is(bodyJSONResponse));
+//    }
+//
+//    @Test
+//    void delete_UserById_NotFound() {
+//        final long userId = 100L;
+//        LocalDateTime dateTime = LocalDateTime.of(2020, 6, 12, 10, 35, 53);
+//        UserNotFoundException exception = new UserNotFoundException(format("User with this userId = %s not exist.", userId));
+//        ApiErrorDto errorDto = new ApiErrorDto.Builder()
+//                .withTimestamp(dateTime)
+//                .withErrorMessage(exception.getMessage())
+//                .withPath("")
+//                .build();
+//
+//        final String expectedJsonResponse = JsonbBuilder.create().toJson(errorDto);
+//
+//        when(dateTimeServiceMock.now()).thenReturn(dateTime);
+//
+//        given()
+//                .pathParam("id", userId)
+//                .when().delete("/users/{id}")
+//                .then()
+//                .statusCode(HttpURLConnection.HTTP_NOT_FOUND)
+//                .body(is(expectedJsonResponse));
+//    }
 
 }
--- src/test/java/org/trl/service/converter/UserConverterTest.java
+++ src/test/java/org/trl/service/converter/UserConverterTest.java
@@ -3,7 +3,7 @@ package org.trl.service.converter;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.trl.controller.dto.UserDTO;
+import org.trl.model.dto.UserDto;
 import org.trl.repository.entity.UserEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,24 +18,23 @@ class UserConverterTest {
     @Inject
     UserConverter userConverter;
 
-    private static UserDTO userDTO_Resource;
+    private static UserDto userDto_Resource;
     private static UserEntity userEntity_Resource;
 
-    private static List<UserDTO> dtoList_Resource;
+    private static List<UserDto> dtoList_Resource;
     private static List<UserEntity> entityList_Resource;
-    private static List<UserEntity> emptyList_Resource;
 
     @BeforeEach
     void setUp() {
 
-        userDTO_Resource = new UserDTO();
-        userDTO_Resource.setId(1L);
-        userDTO_Resource.setFirstName("Roman");
-        userDTO_Resource.setLastName("Tsyupryk");
-        userDTO_Resource.setUsername("TRL");
-        userDTO_Resource.setEmail("tsyupryk.roman@gmail.com");
-        userDTO_Resource.setEmail("strong_password");
-        userDTO_Resource.setBirthday(LocalDate.of(1988, 6, 26));
+        userDto_Resource = new UserDto();
+        userDto_Resource.setId(1L);
+        userDto_Resource.setFirstName("Roman");
+        userDto_Resource.setLastName("Tsyupryk");
+        userDto_Resource.setUsername("TRL");
+        userDto_Resource.setEmail("tsyupryk.roman@gmail.com");
+        userDto_Resource.setEmail("strong_password");
+        userDto_Resource.setBirthday(LocalDate.of(1988, 6, 26));
 
         userEntity_Resource = new UserEntity();
         userEntity_Resource.setId(1L);
@@ -46,22 +45,22 @@ class UserConverterTest {
         userEntity_Resource.setEmail("strong_password");
         userEntity_Resource.setBirthday(LocalDate.of(1988, 6, 26));
 
-        UserDTO userDTO_1 = new UserDTO();
-        userDTO_1.setId(1L);
-        userDTO_1.setFirstName("Roman");
-        userDTO_1.setLastName("Tsyupryk");
-        userDTO_1.setUsername("TRL");
-        userDTO_1.setEmail("tsyupryk.roman@gmail.com");
-        userDTO_1.setEmail("strong_password");
-        userDTO_1.setBirthday(LocalDate.of(1988, 6, 26));
-        UserDTO userDTO_2 = new UserDTO();
-        userDTO_2.setId(2L);
-        userDTO_2.setFirstName("AAA");
-        userDTO_2.setLastName("BBB");
-        userDTO_2.setUsername("AB");
-        userDTO_2.setEmail("AB@gmail.com");
-        userDTO_2.setEmail("strong_password");
-        userDTO_2.setBirthday(LocalDate.of(1900, 1, 1));
+        UserDto userDto_1 = new UserDto();
+        userDto_1.setId(1L);
+        userDto_1.setFirstName("Roman");
+        userDto_1.setLastName("Tsyupryk");
+        userDto_1.setUsername("TRL");
+        userDto_1.setEmail("tsyupryk.roman@gmail.com");
+        userDto_1.setEmail("strong_password");
+        userDto_1.setBirthday(LocalDate.of(1988, 6, 26));
+        UserDto userDto_2 = new UserDto();
+        userDto_2.setId(2L);
+        userDto_2.setFirstName("AAA");
+        userDto_2.setLastName("BBB");
+        userDto_2.setUsername("AB");
+        userDto_2.setEmail("AB@gmail.com");
+        userDto_2.setEmail("strong_password");
+        userDto_2.setBirthday(LocalDate.of(1900, 1, 1));
 
         UserEntity userEntity_1 = new UserEntity();
         userEntity_1.setId(1L);
@@ -80,170 +79,64 @@ class UserConverterTest {
         userEntity_2.setEmail("strong_password");
         userEntity_2.setBirthday(LocalDate.of(1900, 1, 1));
 
-        dtoList_Resource = Collections.unmodifiableList(Arrays.asList(userDTO_1, userDTO_2));
-        entityList_Resource = Collections.unmodifiableList(Arrays.asList(userEntity_1, userEntity_2));
-        emptyList_Resource = Collections.unmodifiableList(new ArrayList<>());
-    }
-
-    // ================================================================================================== mapEntityToDTO
-    @Test
-    void mapEntityToDTO() {
-        UserDTO result = userConverter.mapEntityToDTO(userEntity_Resource);
-        assertEquals(userDTO_Resource, result);
+        dtoList_Resource = List.of(userDto_1, userDto_2);
+        entityList_Resource = List.of(userEntity_1, userEntity_2);
     }
 
     @Test
-    void mapEntityToDTO_NullParameter() {
-        assertThrows(IllegalArgumentException.class, () -> userConverter.mapEntityToDTO(null));
+    void parameterUserEntityIsNull_shouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> userConverter.mapEntityToDto(null));
     }
 
     @Test
-    void mapEntityToDTO_Incorrect_Id() {
-        UserDTO result = userConverter.mapEntityToDTO(userEntity_Resource);
-        result.setId(100L);
-        assertNotEquals(userDTO_Resource, result);
+    void shouldReturnUserDto() {
+        UserDto result = userConverter.mapEntityToDto(userEntity_Resource);
+        assertEquals(userDto_Resource, result);
     }
 
     @Test
-    void mapEntityToDTO_Incorrect_FirstName() {
-        UserDTO result = userConverter.mapEntityToDTO(userEntity_Resource);
-        result.setFirstName("--------------");
-        assertNotEquals(userDTO_Resource, result);
+    void parameterCollectionOfUserEntityIsNull_shouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> userConverter.mapCollectionEntityToCollectionDto(null));
     }
 
     @Test
-    void mapEntityToDTO_Incorrect_LastName() {
-        UserDTO result = userConverter.mapEntityToDTO(userEntity_Resource);
-        result.setLastName("--------------");
-        assertNotEquals(userDTO_Resource, result);
+    void shouldReturnEmptyCollectionOfUserDto() {
+        Collection<UserDto> result = userConverter.mapCollectionEntityToCollectionDto(Collections.emptyList());
+        assertTrue(result.isEmpty());
     }
 
     @Test
-    void mapEntityToDTO_Incorrect_Username() {
-        UserDTO result = userConverter.mapEntityToDTO(userEntity_Resource);
-        result.setUsername("--------------");
-        assertNotEquals(userDTO_Resource, result);
-    }
-
-    @Test
-    void mapEntityToDTO_Incorrect_Email() {
-        UserDTO result = userConverter.mapEntityToDTO(userEntity_Resource);
-        result.setEmail("--------------");
-        assertNotEquals(userDTO_Resource, result);
-    }
-
-    @Test
-    void mapEntityToDTO_Incorrect_Password() {
-        // TODO: Terminate this test.
-    }
-
-    @Test
-    void mapEntityToDTO_Incorrect_Birthday() {
-        UserDTO result = userConverter.mapEntityToDTO(userEntity_Resource);
-        result.setBirthday(LocalDate.of(2000, 1, 1));
-        assertNotEquals(userDTO_Resource, result);
-    }
-
-    // ============================================================================== mapCollectionEntityToCollectionDTO
-    @Test
-    void mapCollectionEntityToCollectionDTO() {
-        Collection<UserDTO> result = userConverter.mapCollectionEntityToCollectionDTO(entityList_Resource);
+    void shouldReturnCollectionWithUserDto() {
+        Collection<UserDto> result = userConverter.mapCollectionEntityToCollectionDto(entityList_Resource);
         assertEquals(dtoList_Resource, result);
     }
 
     @Test
-    void mapCollectionEntityToCollectionDTO_NullParameter() {
-        assertThrows(IllegalArgumentException.class, () -> userConverter.mapCollectionEntityToCollectionDTO(null));
+    void parameterUserDtoIsNull_shouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> userConverter.mapDtoToEntity(null));
     }
 
     @Test
-    void mapCollectionEntityToCollectionDTO_Check_Failure_Of_Test() {
-        Collection<UserDTO> result = userConverter.mapCollectionEntityToCollectionDTO(entityList_Resource);
-        result.add(new UserDTO());
-        assertNotEquals(dtoList_Resource, result);
-    }
-
-    @Test
-    void mapCollectionEntityToCollectionDTO_Empty_Collection() {
-        Collection<UserDTO> result = userConverter.mapCollectionEntityToCollectionDTO(emptyList_Resource);
-        assertEquals(emptyList_Resource, result);
-    }
-
-    // ================================================================================================== mapDTOToEntity
-    @Test
-    void mapDTOToEntity() {
-        UserEntity result = userConverter.mapDTOToEntity(userDTO_Resource);
+    void shouldReturnUserEntity() {
+        UserEntity result = userConverter.mapDtoToEntity(userDto_Resource);
         assertEquals(userEntity_Resource, result);
     }
 
     @Test
-    void mapDTOToEntity_NullParameter() {
-        assertThrows(IllegalArgumentException.class, () -> userConverter.mapDTOToEntity(null));
+    void parameterCollectionOfUserDtoIsNull_shouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> userConverter.mapCollectionDtoToCollectionEntity(null));
     }
 
     @Test
-    void mapDTOToEntity_Incorrect_Id() {
-        UserEntity result = userConverter.mapDTOToEntity(userDTO_Resource);
-        result.setId(100L);
-        assertNotEquals(userEntity_Resource, result);
+    void shouldReturnEmptyCollectionOfUserEntity() {
+        Collection<UserEntity> result = userConverter.mapCollectionDtoToCollectionEntity(Collections.emptyList());
+        assertTrue(result.isEmpty());
     }
 
     @Test
-    void mapDTOToEntity_Incorrect_FirstName() {
-        UserEntity result = userConverter.mapDTOToEntity(userDTO_Resource);
-        result.setFirstName("--------------");
-        assertNotEquals(userEntity_Resource, result);
-    }
-
-    @Test
-    void mapDTOToEntity_Incorrect_LastName() {
-        UserEntity result = userConverter.mapDTOToEntity(userDTO_Resource);
-        result.setLastName("--------------");
-        assertNotEquals(userEntity_Resource, result);
-    }
-
-    @Test
-    void mapDTOToEntity_Incorrect_Username() {
-        UserEntity result = userConverter.mapDTOToEntity(userDTO_Resource);
-        result.setUsername("--------------");
-        assertNotEquals(userEntity_Resource, result);
-    }
-
-    @Test
-    void mapDTOToEntity_Incorrect_Email() {
-        UserEntity result = userConverter.mapDTOToEntity(userDTO_Resource);
-        result.setEmail("--------------");
-        assertNotEquals(userEntity_Resource, result);
-    }
-
-    @Test
-    void mapDTOToEntity_Incorrect_Password() {
-        // TODO: Terminate this test...
-    }
-
-    @Test
-    void mapDTOToEntity_Incorrect_Birthday() {
-        UserEntity result = userConverter.mapDTOToEntity(userDTO_Resource);
-        result.setBirthday(LocalDate.of(2000, 1, 1));
-        assertNotEquals(userEntity_Resource, result);
-    }
-
-    // ============================================================================== mapCollectionDTOToCollectionEntity
-    @Test
-    void mapCollectionDTOToCollectionEntity() {
-        Collection<UserEntity> result = userConverter.mapCollectionDTOToCollectionEntity(dtoList_Resource);
+    void shouldReturnCollectionWithUserEntities() {
+        Collection<UserEntity> result = userConverter.mapCollectionDtoToCollectionEntity(dtoList_Resource);
         assertEquals(entityList_Resource, result);
     }
 
-    @Test
-    void mapCollectionDTOToCollectionEntity_NullParameter() {
-        assertThrows(IllegalArgumentException.class, () -> userConverter.mapCollectionDTOToCollectionEntity(null));
-    }
-
-    @Test
-    void mapCollectionDTOToCollectionEntity_Check_Failure_Of_Test() {
-        Collection<UserEntity> result = userConverter.mapCollectionDTOToCollectionEntity(dtoList_Resource);
-        result.add(new UserEntity());
-        assertNotEquals(entityList_Resource, result);
-    }
 }
